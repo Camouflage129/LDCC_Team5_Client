eba7b93c997ac3ea3140d2c8313d4d508e88a6a1
Camouflage129 <phh129@hanmail.net>
2018-08-02 16:24:32 +0900

    기사님 택배량

--- LDCC_Team5_Client/WebContent/contents/INF/js/INF0001.js
+++ LDCC_Team5_Client/WebContent/contents/INF/js/INF0001.js
@@ -5,11 +5,7 @@ var page = {
 	},
 
 	initInterface : function(data) {
-		$("#desBtn").click(function() {
-			LEMP.Window.open({
-				"_sPagePath" : "LIST/html/LIST0001.html"
-			});
-		})
+		
 	},
 
 	initLayout : function() {
@@ -19,5 +15,21 @@ var page = {
 		$("#name").append('<b>'+login.name+'</b>');
 		$("#date").append(login.date);
 		$("#region").append(login.region);
+		
+		$.ajax({
+			type : 'GET',
+			url : 'http://52.79.44.163:8080/LDCC_Team5_Server/userStatus/'+login.id+'/'+today,
+		    success : function(result)  {
+		    	var i = 0;
+		    	var list = [];
+		    	$.each(result, function(key, value) {
+		    		list[i++] = value;
+		    	});
+		    	$("#remainBox").append(list[0]);
+		    	$("#totalBox").append(list[1]);
+		    	$("#percent").append((list[0]/list[1])*100);
+		    	$("#percent").css("style","width:"+(list[0]/list[1])*100+"%");
+		    }
+		});
 	}
 }
