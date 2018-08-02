98f7c298c1ba58ec601ab7afa135d8b53f3ae518
LENOVO <LENOVO@DESKTOP-6FICUGS>
2018-08-02 17:46:23 +0900

    자잘한수정..

--- LDCC_Team5_Client/WebContent/contents/INF/js/INF0001.js
+++ LDCC_Team5_Client/WebContent/contents/INF/js/INF0001.js
@@ -16,6 +16,19 @@ var page = {
 		$("#date").append(login.date);
 		$("#region").append(login.region);
 		
+		var today = new Date();
+		var dd = today.getDate();
+		var mm = today.getMonth()+1;
+		var yyyy = today.getFullYear();
+	
+		if(dd<10) {
+		    dd='0'+dd
+		} 
+		if(mm<10) {
+		    mm='0'+mm
+		} 
+		today = yyyy+'-'+mm+'-'+dd;
+		
 		$.ajax({
 			type : 'GET',
 			url : 'http://52.79.44.163:8080/LDCC_Team5_Server/userStatus/'+login.id+'/'+today,
