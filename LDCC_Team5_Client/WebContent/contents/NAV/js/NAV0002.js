54f7e6c011420fcc760d609ce508842213909f5f
Camouflage129 <phh129@hanmail.net>
2018-08-03 17:27:44 +0900

    끝..ㅎㅎ

--- LDCC_Team5_Client/WebContent/contents/NAV/js/NAV0002.js
+++ LDCC_Team5_Client/WebContent/contents/NAV/js/NAV0002.js
@@ -1,26 +1,6 @@
 var page = {
 	init : function(args) {
-		
-		//<![CDATA[
-	    // 사용할 앱의 JavaScript 키를 설정해 주세요.
-	    Kakao.init('b7dc1ebcfea8f68fff20eeb74d7fba21');
-	    navi();
-	    // 카카오 로그인 버튼을 생성합니다.
-	   
-	  //]]>
-		
-	    
-	    
-	    
+		$('#load').hide();
+		location.href="https://kakaonavi-wguide.kakao.com/navigate.html?appkey=3e786f8df14fcfc89d159421a6a7c9b6&apiver=1.0&extras=%7B%22KA%22%3A%22sdk%2F1.24.1%20os%2Fjavascript%20lang%2Fko-KR%20device%2FWin32%20origin%2Fhttps%253A%252F%252Fdevelopers.kakao.com%22%7D&param=%7B%22destination%22%3A%7B%22name%22%3A%22%ED%98%84%EB%8C%80%EB%B0%B1%ED%99%94%EC%A0%90%20%ED%8C%90%EA%B5%90%EC%A0%90%22%2C%22x%22%3A127.11205203011632%2C%22y%22%3A37.39279717586919%7D%2C%22option%22%3A%7B%22coord_type%22%3A%22wgs84%22%7D%7D";
 	}
-
-}
-
-function navi(){
-    Kakao.Navi.start({
-        name: "1 목적지",
-        x: 126.9453253069999,
-        y: 37.55596209855752,
-        coordType: 'wgs84'
-    });
 }
